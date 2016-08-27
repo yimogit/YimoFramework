de6b63be36b0c519b44e801b1150c1447042ac41
yimo <507382865@qq.com>
2016-08-28 00:57:01 +0800

    后台模板

---
+++ demo/WebUI/WebUI/Content/admin/custom.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    $("#side-menu .J_menuItem").click(function () {
+        $("#side-menu .J_menuItem.current").removeClass("current");
+        $(this).addClass("current")
+    });
+
+})
