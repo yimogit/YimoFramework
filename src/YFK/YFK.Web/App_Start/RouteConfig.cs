5684174046199d7d1bf31411af1c5bafa9ee7548
yimo <507382865@qq.com>
2016-08-28 14:28:15 +0800

    添加自定义配置文件

--- src/YFK/YFK.Web/App_Start/RouteConfig.cs
+++ src/YFK/YFK.Web/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using YFK.Core.Configuration;
 using YFK.MvcFramework.RouteExtensions;
 
 namespace YFK.Web
@@ -13,21 +14,21 @@ namespace YFK.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
-            const string adminDomin="admin.yimo.dev";
-            const string webDomin="www.yimo.dev";
+
+            string adminDomin = YFKConfig.Instance.WebSetting.AdminDomin;// "admin.yimo.dev";
+            string webDomin = YFKConfig.Instance.WebSetting.MainDomin;// "www.yimo.dev";
             //上面的域名需要在IIS中绑定
             routes.MapAreaRoute(
                 "YFK.Admin",
                 routes.MapRouteDomain(
-                    name: "Admin/Home/Index",
+                    name: "domin/Admin/Home/Index",
                     domain: adminDomin,
                     url: "{controller}/{action}/{id}",
                     defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
                     namespaces: new string[] { "YFK.Admin.Controllers" }
                 )
             );
-
+            
             routes.MapRouteDomain(
                 name: "Web/Home/Index",
                 domain: webDomin,
